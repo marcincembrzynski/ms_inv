555cd8361bcaefb85b93b2342cea1e81b99004e1
Marcin <marcin@inandi.com>
2018-02-26 21:04:33 +0000

    test populate db

--- ms_inv_test.erl
+++ ms_inv_test.erl
@@ -1,6 +1,7 @@
 -module(ms_inv_test).
 -export([test/2]).
 -export([start/1]).
+-export([populate_db/1]).
 
 start(N) -> 
 	ms_inv_test:test(N, erlang:timestamp()).
@@ -14,3 +15,21 @@ test(N, StartTime) ->
 	ms_inv:add(1,pl,1),
 	ms_inv:remove(1,pl,1),
 	test(N - 1, StartTime).
+
+populate_db() -> populate_db(inventory_db).
+
+
+% 100000 % 4
+populate_db(DBName) -> 
+	{ok, DB} = dets:open_file(DBName, [{type, set}, {file, DBName}]), 
+	Warehouses = [pl,de,uk,fr],
+
+	Populate = fun({P,W}) ->
+		dets:insert(DB, {{P,W}, P, 1})
+	end,
+	
+	List = [{P,W} || P <- lists:seq(1,100000), W <- [pl,uk,de,fr]],
+	lists:foreach(Populate, List), 
+	io:format("info: ~p~n", [dets:info(DB)]).
+
+

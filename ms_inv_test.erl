4e199f4b89d3b85704638eb18ed18aa856276352
marcincembrzynski <marcin@inandi.com>
2018-03-01 20:58:45 +0000

    supervisor

--- ms_inv_test.erl
+++ ms_inv_test.erl
@@ -1,13 +1,14 @@
 -module(ms_inv_test).
 -export([test/2]).
+-export([test2/0]).
 -export([start/1]).
--export([populate_db/1]).
 -export([populate_db/0]).
 
 start(N) -> 
 	ms_inv_test:test(N, erlang:timestamp()).
 
-test(0, StartTime) -> 
+test(0, StartTime) ->
+	{ok, {1,pl,1,_}} = ms_inv:get(1,pl),
 	Time = timer:now_diff(erlang:timestamp(),StartTime),
 	io:format("time: ~p~n", [Time]);
 	
@@ -17,13 +18,13 @@ test(N, StartTime) ->
 	ms_inv:remove(1,pl,1),
 	test(N - 1, StartTime).
 
-populate_db() -> populate_db(inventory_db).
+
 
 
 % 100000 % 4
-populate_db(DBName) -> 
+populate_db() ->
+	[DBName|_] = string:split(atom_to_list(node()),"@"),
 	{ok, DB} = dets:open_file(DBName, [{type, set}, {file, DBName}]), 
-	Warehouses = [pl,de,uk,fr],
 
 	Populate = fun({P,W}) ->
 		dets:insert(DB, {{P,W}, P, 1})
@@ -32,5 +33,13 @@ populate_db(DBName) ->
 	List = [{P,W} || P <- lists:seq(1,100000), W <- [pl,uk,de,fr]],
 	lists:foreach(Populate, List), 
 	io:format("info: ~p~n", [dets:info(DB)]).
+
+
+test2() ->
+	{ok, {4,de,4, _}} = ms_inv:get(4,de), 
+	{ok, {4,de,5, _}} = ms_inv:add(4,de,1),
+	{ok, {4,de,5, _}} = ms_inv:get(4,de), 
+	{ok, {4,de,4, _}} = ms_inv:remove(4,de,1),
+	{ok, {4,de,4, _}} = ms_inv:get(4,de).
 
 

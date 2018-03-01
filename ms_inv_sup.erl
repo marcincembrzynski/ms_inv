5762a9c9b6dd4645c1e8437f189e41136317a18b
marcincembrzynski <marcin@inandi.com>
2018-03-01 20:17:58 +0000

    supervisor

---
+++ ms_inv_sup.erl
@@ -0,0 +1,17 @@
+-module(ms_inv_sup).
+-behavior(supervisor).
+-export([start_link/0, init/1]).
+-export([stop/0]).
+
+start_link() ->
+  supervisor:start_link({local,?MODULE},?MODULE, []).
+
+stop() ->
+  exit(whereis(?MODULE), shutdown).
+
+init(_) ->
+  ChildSpecList = [child(ms_inv)], {ok,{{one_for_one, 1, 10}, ChildSpecList}}.
+
+child(Module) ->
+  {Module, {Module, start_link, []},
+    permanent, 2000, worker, [Module]}.

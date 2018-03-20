d7b0ec1fd5fb32ce6c156d16de9e77c70197e7f4
marcincembrzynski <marcin@inandi.com>
2018-03-20 21:54:29 +0000

    rename

---
+++ testdb.erl
@@ -0,0 +1,21 @@
+-module(testdb).
+-export([create/0]).
+
+
+
+% 100000 % 4
+create() ->
+	[DBName|_] = string:split(atom_to_list(node()),"@"),
+	{ok, DB} = dets:open_file(DBName, [{type, set}, {file, DBName}]), 
+
+	Populate = fun({P,W}) ->
+		dets:insert(DB, {{P,W}, P, 1})
+	end,
+	
+	List = [{P,W} || P <- lists:seq(1,100000), W <- [pl,uk,de,fr]],
+	lists:foreach(Populate, List), 
+	io:format("info: ~p~n", [dets:info(DB)]).
+
+
+
+

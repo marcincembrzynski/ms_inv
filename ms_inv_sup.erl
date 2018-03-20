ed3bab84742b78379862ee4a2e25469a4cfe10e8
marcincembrzynski <marcin@inandi.com>
2018-03-20 21:25:17 +0000

    3

--- ms_inv_sup.erl
+++ ms_inv_sup.erl
@@ -10,7 +10,7 @@ stop() ->
   exit(whereis(?MODULE), shutdown).
 
 init(_) ->
-  ChildSpecList = [child(ms_inv)], {ok,{{one_for_one, 1, 10}, ChildSpecList}}.
+  ChildSpecList = [child(ms_db), child(ms_inv)], {ok,{{one_for_one, 1, 10}, ChildSpecList}}.
 
 child(Module) ->
   {Module, {Module, start_link, []},

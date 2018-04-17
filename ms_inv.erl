a9bb0a0089309ab30e20cc7c0624b926777813bb
marcincembrzynski <marcin@inandi.com>
2018-04-17 16:47:01 +0100

    clean

--- ms_inv.erl
+++ ms_inv.erl
@@ -1,8 +1,11 @@
 -module(ms_inv).
 -export([start_link/0,init/1,stop/0]).
--export([get/2,add/3,remove/3]).
--export([handle_call/3,handle_cast/2,terminate/2,logs/0]).
+-export([get/2,get/3,add/3,add/4,remove/3,remove/4]).
+-export([handle_call/3,handle_cast/2,terminate/2]).
 -behaviour(gen_server).
+-record(inventoryKey, {productId, warehouseId}).
+-record(inventory, {inventoryKey = #inventoryKey{}, quantity}).
+
 
 
 start_link() ->
@@ -11,23 +14,32 @@ start_link() ->
 
 init(_Args) ->
   process_flag(trap_exit, true),
-  LogTable = ets:new(logTable, []),
+  LogTable = ets:new(logTable, [public]),
+  pg2:create(ms_inv),
+  pg2:join(ms_inv, self()),
   {ok, [LogTable]}.
 
 stop() -> gen_server:cast(?MODULE, stop).
 
-terminate(_Reason, DB) ->
-  dets:close(DB),
+terminate(_Reason, _) ->
+  pg2:leave(ms_inv, self()),
   ok.
 
 call(Msg) -> 
   gen_server:call(?MODULE, Msg).
 
+call(Node, Msg) ->
+  gen_server:call({?MODULE,Node}, Msg).
+
 %% returns 
 %% {ok, {ProductId, CountryId, Quantity, Version}}
 %% {error, not_found}
-get(ProductId, CountryId) -> 
+get(ProductId, CountryId) ->
   call({get, {ProductId, CountryId}}).
+
+
+get(Node, ProductId, CountryId) ->
+  call(Node, {get, {ProductId, CountryId}}).
 
 
 %% subtracts given quantity from the product inventory
@@ -38,6 +50,9 @@ get(ProductId, CountryId) ->
 remove(ProductId, CountryId, Quantity) ->
   call({remove,{ProductId, CountryId, Quantity}}).
 
+remove(Node, ProductId, CountryId, Quantity) ->
+  call(Node, {remove,{ProductId, CountryId, Quantity}}).
+
 
 %% adds given quantity to the product inventory
 %% returns 
@@ -46,24 +61,19 @@ remove(ProductId, CountryId, Quantity) ->
 add(ProductId, CountryId, Quantity) -> 
   call({add,{ProductId, CountryId, Quantity}}).
 
+add(Node, ProductId, CountryId, Quantity) ->
+  call(Node, {add,{ProductId, CountryId, Quantity}}).
 
-logs() ->
-  call(logs).
 
 handle_call({get, {ProductId, CountryId}}, _From, LoopData) ->
   {reply, get_inventory(ProductId, CountryId), LoopData};
 
 handle_call({remove, {ProductId, CountryId, RemoveQuantity}}, From, LoopData) ->
-
   {reply, remove_inventory(ProductId, CountryId, RemoveQuantity, From, LoopData), LoopData};
 
 
 handle_call({add, {ProductId, CountryId, AddQuantity}}, From, LoopData) ->
-  {reply, add_inventory(ProductId, CountryId, AddQuantity, From, LoopData), LoopData};
-
-
-handle_call(logs, _From, LoopData) ->
-  {reply, logs(LoopData), LoopData}.
+  {reply, add_inventory(ProductId, CountryId, AddQuantity, From, LoopData), LoopData}.
 
 handle_cast(stop, LoopData) ->
   {stop, normal, LoopData}.
@@ -81,12 +91,9 @@ get_inventory(ProductId, CountryId) ->
 
 
 
-remove_inventory(ProductId, CountryId, RemoveQuantity, From, LoopData) ->
+remove_inventory(ProductId, CountryId, RemoveQuantity, _From, _LoopData) ->
 
   Response = ms_db:read({ProductId,CountryId}),
-  {ok, {_Key, Value, _Version}} = Response,
-  log_insert({'-', br, Value, From}, LoopData),
-  %%io:format("- br: ~p~n", [Value]),
 
   case Response of
     {ok, {_Key,0, _Version}} ->
@@ -94,8 +101,6 @@ remove_inventory(ProductId, CountryId, RemoveQuantity, From, LoopData) ->
 
     {ok, {Key,Quantity, _}} ->
       {ok, {{ProductId, CountryId}, NewQuantity, _}} = ms_db:write(Key, Quantity - RemoveQuantity),
-      %io:format("- ar: ~p~n", [NewQuantity]),
-      log_insert({'-', ar, NewQuantity, From}, LoopData),
       {ok, {ProductId, CountryId, NewQuantity}};
 
     {error, Error} ->
@@ -104,30 +109,15 @@ remove_inventory(ProductId, CountryId, RemoveQuantity, From, LoopData) ->
 
 
 
-add_inventory(ProductId, CountryId, AddQuantity, From, LoopData) ->
+add_inventory(ProductId, CountryId, AddQuantity, _From, _LoopData) ->
 
   Response = ms_db:read({ProductId,CountryId}),
-  {ok, {_Key, Value, _Version}} = Response,
-  log_insert({'+', ba, Value, From}, LoopData),
-  %io:format("+ ba: ~p~n", [Value]),
 
   case Response of
     {ok, {Key,Quantity,_}} ->
       {ok, {{ProductId, CountryId}, NewQuantity, _}} = ms_db:write(Key, Quantity + AddQuantity),
-      %io:format("+ aa: ~p~n", [NewQuantity]),
-      log_insert({'+', aa, NewQuantity, From}, LoopData),
       {ok, {ProductId, CountryId, NewQuantity}};
 
     {error, Error} ->
       {error, Error}
   end.
-
-
-log_insert(Value, LoopData) ->
-  %%io:format("loopdata ~p~n", [LoopData]),
-  [LogTable] = LoopData,
-   ets:insert(LogTable, {erlang:timestamp(), Value}).
-
-logs(LoopData) ->
-  [LogTable] = LoopData,
-  ets:tab2file(LogTable, "logs.txt").

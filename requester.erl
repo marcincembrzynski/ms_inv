5c9768362b549e4339f89b8e0804acb44f715835
marcincembrzynski <marcin@inandi.com>
2018-04-19 20:15:31 +0100

    modify test

--- requester.erl
+++ requester.erl
@@ -1,5 +1,5 @@
 -module(requester).
--export([start/2,loop/0,to_ets/1]).
+-export([start/2,loop/0,test/0]).
 -record(testData, {startTime, dbRef, productId, warehouseId}).
 
 
@@ -7,7 +7,7 @@ start(Processes, Requests) ->
   {ok, [{inventory,{ProductId,WarehouseId}}]} = file:consult("requester.txt"),
   {ok, DB} = dets:open_file(requests_db, [{type, set}, {file, requests_db}]),
   dets:delete_all_objects(DB),
-  dets:insert(DB, {erlang:timestamp(), start, ms_inv_client:get(ProductId, WarehouseId)}),
+  dets:insert(DB, {erlang:timestamp(), start, ms_inv_proxy:get(ProductId, WarehouseId)}),
   TestData = #testData{ startTime = erlang:timestamp(), dbRef = DB, productId = ProductId, warehouseId = WarehouseId},
   io:format("data: ~p~n",[TestData]),
   init(Processes, Requests, TestData).
@@ -20,16 +20,18 @@ init(Processes,Requests, TestData) ->
   NewProcesses = Processes - 1,
   init(NewProcesses, Requests, TestData).
 
-to_ets(Name) ->
+test() ->
   {ok, DB} = dets:open_file(requests_db, [{type, set}, {file, requests_db}]),
-  LogTable = ets:new(Name, [public]),
+  LogTable = ets:new(requestes_ets, [public]),
   dets:to_ets(DB, LogTable),
+
 
   List = ets:tab2list(LogTable),
   TimeStampSort = fun({T1 ,_ ,_ },{T2 ,_, _}) -> T1 =< T2 end,
 
   SortedList = lists:sort(TimeStampSort, List),
-
+  Operations = length(SortedList) - 1,
+  
   [Start|Tail] = SortedList,
 
   {_,start, {ok,{_,_,StartQuantity}}} = Start,
@@ -40,7 +42,6 @@ to_ets(Name) ->
       {_, remove, {ok, {_, _, NewQuantity}}} ->
         true  = (PreviousQuantity - 1 == NewQuantity),
         NewQuantity;
-
 
       {_, add, {ok, {_, _, NewQuantity}}} ->
 
@@ -53,8 +54,20 @@ to_ets(Name) ->
    end,
 
   Acc = lists:foldl(CorrectnessFun, StartQuantity, Tail),
+  Last = lists:last(SortedList),
+  Seconds = calculate_seconds(Start, Last),
 
-  {Acc, Start}.
+  io:format("sorted list length, number of operations: ~p~n", [Operations]),
+  io:format("Time taken: ~p~n", [Seconds]),
+  io:format("Operations per second: ~p~n", [Operations/Seconds]),
+
+  {acc, Acc, start, Start, last, Last, time, Seconds}.
+
+calculate_seconds(Start, Last) ->
+  {StartTimestamp, _, _} = Start,
+  {LastTimestamp, _, _} = Last,
+  Time = timer:now_diff(LastTimestamp, StartTimestamp),
+  Time / 1000000.
 
 
 loop() ->

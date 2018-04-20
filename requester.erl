ede5c09c193c6af38ab7f8eb48c4481924dbfbaa
marcincembrzynski <marcin@inandi.com>
2018-04-20 19:22:47 +0100

    adding failures.

--- requester.erl
+++ requester.erl
@@ -1,14 +1,16 @@
 -module(requester).
--export([start/2,loop/0,test/0]).
--record(testData, {startTime, dbRef, productId, warehouseId}).
+-export([start/2,loop/0,test/0,step/2]).
+-record(testData, {startTime, dbRef, productId, warehouseId, counterRef}).
 
 
 start(Processes, Requests) ->
   {ok, [{inventory,{ProductId,WarehouseId}}]} = file:consult("requester.txt"),
   {ok, DB} = dets:open_file(requests_db, [{type, set}, {file, requests_db}]),
+  CounterTable = ets:new(counter, [public]),
+  ets:insert(CounterTable, {counter,0}),
   dets:delete_all_objects(DB),
   dets:insert(DB, {erlang:timestamp(), start, ms_inv_proxy:get(ProductId, WarehouseId)}),
-  TestData = #testData{ startTime = erlang:timestamp(), dbRef = DB, productId = ProductId, warehouseId = WarehouseId},
+  TestData = #testData{ startTime = erlang:timestamp(), dbRef = DB, productId = ProductId, warehouseId = WarehouseId, counterRef = CounterTable},
   io:format("data: ~p~n",[TestData]),
   init(Processes, Requests, TestData).
 
@@ -31,7 +33,7 @@ test() ->
 
   SortedList = lists:sort(TimeStampSort, List),
   Operations = length(SortedList) - 1,
-  
+
   [Start|Tail] = SortedList,
 
   {_,start, {ok,{_,_,StartQuantity}}} = Start,
@@ -48,7 +50,9 @@ test() ->
         true = (PreviousQuantity + 1 == NewQuantity),
         NewQuantity;
 
-      {error, _} -> PreviousQuantity
+      {_, add, {error,_}} -> PreviousQuantity;
+
+      {_, remove, {error,_}} -> PreviousQuantity
 
     end
    end,
@@ -83,12 +87,17 @@ loop() ->
 
     {requests, N, TestData} ->
 
+      %% stop node
+
+      stop_node(TestData),
+
 
       ProductId = TestData#testData.productId,
       WarehouseId = TestData#testData.warehouseId,
       DBRef = TestData#testData.dbRef,
       ResponseRemove = ms_inv_proxy:remove(ProductId, WarehouseId,1),
       ok = dets:insert(DBRef, {erlang:timestamp(), remove, ResponseRemove}),
+      ets:update_counter(TestData#testData.counterRef, counter, 1),
 
       case ResponseRemove of
         {ok, _} -> ok;
@@ -100,16 +109,32 @@ loop() ->
 
       Response = ms_inv_proxy:add(ProductId, WarehouseId,1),
       ok = dets:insert(DBRef, {erlang:timestamp(), add, Response}),
+      ets:update_counter(TestData#testData.counterRef, counter, 1),
       case Response of
         {ok, _Data} -> ok;
         {error, Error} -> io:format("add error ~p~n", [Error])
       end,
 
-
+      io:format("## counter: ~p~n", [ets:lookup(TestData#testData.counterRef, counter)]),
 
       self() ! {requests, N - 1, TestData},
       loop()
 
   end.
 
+stop_node(TestData) ->
+  [{counter, Counter}] = ets:lookup(TestData#testData.counterRef, counter),
+  IsStep = step(Counter, 10000),
+  case IsStep of
+    true ->
+      io:format("### stopping node, ~n"),
+      ms_inv_proxy:stop_node();
+    false ->
+      ok
+  end,
+  TestData.
 
+
+
+step(X,Step) when X < Step -> false;
+step(X,Step) -> ceil(X / Step) == floor(X / Step).

ddd5883e7c48c1aae7d22574c28222a17365527c
marcincembrzynski <marcin@inandi.com>
2018-04-22 18:37:15 +0100

    fix

--- ms_inv_test.erl
+++ ms_inv_test.erl
@@ -1,49 +1,78 @@
 -module(ms_inv_test).
--export([test/2]).
--export([test2/0]).
--export([start/1, start/2]).
--export([populate_db/0]).
+-export([start_link/0,init/1,stop/0,handle_cast/2,handle_call/3]).
+-export([test/1]).
+-record(testData, {startTime, productId, warehouseId}).
 
 
-start(Processes, Requests) ->
+start_link() ->
+  gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 
-start(N) -> 
-	ms_inv_test:test(N, erlang:timestamp()).
-
-test(0, StartTime) ->
-	{ok, {1,pl,1,_}} = ms_inv:get(1,pl),
-	Time = timer:now_diff(erlang:timestamp(),StartTime),
-	io:format("time: ~p~n", [Time]);
-	
-
-test(N, StartTime) -> 
-	ms_inv:add(1,pl,1),
-	ms_inv:remove(1,pl,1),
-	test(N - 1, StartTime).
+init(_Args) ->
+  {ok, DB} = dets:open_file(msinvtestdb, [{type, set}, {file, msinvtestdb}]),
+  Response = dets:lookup(DB, requests),
+  case Response of
+    [{response, 0}] -> {ok, [DB]};
+    [] -> {ok, DB}
+  end,
+  {ok, [DB]}.
 
 
+call(Msg) ->
+  gen_server:call(?MODULE, Msg).
 
 
-% 100000 % 4
-populate_db() ->
-	[DBName|_] = string:split(atom_to_list(node()),"@"),
-	{ok, DB} = dets:open_file(DBName, [{type, set}, {file, DBName}]), 
+stop() ->
+  gen_server:cast(?MODULE, stop).
 
-	Populate = fun({P,W}) ->
-		dets:insert(DB, {{P,W}, P, 1})
-	end,
-	
-	List = [{P,W} || P <- lists:seq(1,100000), W <- [pl,uk,de,fr]],
-	lists:foreach(Populate, List), 
-	io:format("info: ~p~n", [dets:info(DB)]).
+test(N) ->
+  call({test, N}).
 
 
-test2() ->
-	{ok, {4,de,4, _}} = ms_inv:get(4,de), 
-	{ok, {4,de,5, _}} = ms_inv:add(4,de,1),
-	{ok, {4,de,5, _}} = ms_inv:get(4,de), 
-	{ok, {4,de,4, _}} = ms_inv:remove(4,de,1),
-	{ok, {4,de,4, _}} = ms_inv:get(4,de).
+handle_cast(stop, LoopData) ->
+  {stop, normal, LoopData}.
 
+
+handle_call({test, N}, _From, LoopData) ->
+  {reply, test_init(N, LoopData), LoopData}.
+
+test_init(N, LoopData) ->
+  io:format("### init test: ~n"),
+  TestData = #testData{ startTime = erlang:timestamp(), productId = 9999, warehouseId = uk},
+  test(N, TestData, LoopData).
+
+test(0, TestData, LoopData) ->
+  [DB] = LoopData,
+  ok = dets:insert(DB, {requests, 0}),
+  ProductId = TestData#testData.productId,
+  WarehouseId = TestData#testData.warehouseId,
+  io:format("#~p~n",[ms_inv_proxy:get(ProductId,WarehouseId)]),
+  Time = timer:now_diff(erlang:timestamp(), TestData#testData.startTime),
+  Seconds = Time / 1000000,
+  io:format("time: ~p~n", [Seconds]);
+
+
+test(N, TestData, LoopData) ->
+  [DB] = LoopData,
+  ok = dets:insert(DB, {requests, N}),
+
+  ProductId = TestData#testData.productId,
+  WarehouseId = TestData#testData.warehouseId,
+  ResponseRemove = ms_inv_proxy:remove(ProductId, WarehouseId,1),
+  case ResponseRemove of
+    {ok, _} -> ok;
+    {error, _} -> io:format("remove error ~p~n", [ResponseRemove])
+  end,
+
+
+
+  Response = ms_inv_client:add(ProductId, WarehouseId,1),
+  case Response of
+    {ok, _Data} -> ok;
+    {error, Error} -> io:format("add error ~p~n", [Error])
+  end,
+
+
+  Requests = N - 1,
+  test(Requests, TestData, LoopData).
 

d07f75bb4b0a144510c9a1c50913974e2b53e2d6
marcincembrzynski <marcin@inandi.com>
2018-08-15 09:43:03 +0200

    test status

--- test_status.erl
+++ test_status.erl
@@ -1,7 +1,7 @@
 -module(test_status).
--export([start/2,loop/0, stop_node/0, result/0]).
+-export([start/3,loop/0, stop_node/1, result/0]).
 
-start(Proc, Req) ->
+start(Proc, Req, Interval) ->
 
     ok = ms_inv_proxy:status(),
 
@@ -12,13 +12,19 @@ start(Proc, Req) ->
         ets:delete_all_objects(?MODULE)
     end,
 
-    Pid = spawn(?MODULE, stop_node, []),
-    Pid ! {stop_node, 1},
-
+    init_stop_node(Interval),
 
     init(Proc,Req, Pid).
 
+init_stop_node(Interval) ->
+  MilisecondsInterval = Interval * 1000,
+  Pid = spawn(?MODULE, stop_node, [MilisecondsInterval]),
+  Pid ! {stop_node, 1}.
+
+
 init(0, _, _) -> ok;
+
+
 
 init(Proc, Req, StopNodePid) ->
   Pid = spawn(?MODULE, loop, []),
@@ -43,20 +49,22 @@ loop() ->
   end.
 
 
-stop_node() ->
+stop_node(Interval) ->
   receive
 
     {stop} ->
-      io:format("stoping stop node: ~n"),
+      io:format("stoping stop_node process ~n"),
       ok;
 
     {stop_node, N} ->
-      io:format("Stop Node process sleeps.... ~n"),
-      timer:sleep(5000),
+      io:format("stop_node process sleeps.... ~n"),
+      %% sleep time as argument
+      timer:sleep(Interval),
       io:format("stoping node event number: ~p~n", [N]),
+      io:format("--------------------------------- ~n"),
       ms_inv_proxy:stop_node(),
       self() ! {stop_node, N + 1},
-      stop_node()
+      stop_node(Interval)
 
 
   end.

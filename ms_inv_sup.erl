26514e6a4318a125b812b801527b886b2bafa280
marcincembrzynski <marcin@inandi.com>
2018-08-15 09:43:22 +0200

    test status

--- ms_inv_sup.erl
+++ ms_inv_sup.erl
@@ -4,13 +4,15 @@
 -export([stop/0]).
 
 start_link() ->
-  supervisor:start_link({local,?MODULE},?MODULE, []).
+  supervisor:start_link({local,?MODULE}, ?MODULE, []).
 
 stop() ->
+  io:format("### stopping ~n"),
   exit(whereis(?MODULE), shutdown).
 
+
 init(_) ->
-  ChildSpecList = [child(ms_db, [ms_inv_db]), child(ms_inv, [])], {ok,{{one_for_one, 1, 1}, ChildSpecList}}.
+  ChildSpecList = [child(ms_db, [ms_inv_db]), child(ms_inv, [])], {ok,{{one_for_one, 1, 5}, ChildSpecList}}.
 
 child(Module, Args) ->
   {Module, {Module, start_link, Args},

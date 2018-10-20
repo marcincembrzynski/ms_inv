c8aa5239d92ed1595d98babfaf394a310e242f26
marcincembrzynski <marcin@inandi.com>
2018-10-20 10:39:03 +0100

    ms_prod

--- ms_prod.erl
+++ ms_prod.erl
@@ -1,6 +1,6 @@
 -module(ms_prod).
 -behaviour(gen_server).
--record(loopData, {dbname, dbref, dbContentName, dbContentRef}).
+-record(loopData, {dbname, dbref, dbContentName, dbContentRef, msProdNodes}).
 
 -export([start_link/0,init/1,stop/0,stop/1,handle_call/3,handle_cast/2]).
 -export([get/4]).
@@ -12,13 +12,17 @@ get(Node, ProductId, CountryId, LanguageId) ->
 start_link() ->
   [DBName|_] = string:split(atom_to_list(node()),"@"),
   DBContentName = string:concat(DBName, "_content"),
-  LoopData = #loopData{dbname = DBName, dbContentName = DBContentName},
+  {ok,[ProductNodes]} = file:consult(ms_prod_nodes),
+  LoopData = #loopData{dbname = DBName, dbContentName = DBContentName, msProdNodes = ProductNodes},
   gen_server:start_link({local, ?MODULE}, ?MODULE, LoopData, []).
 
 init(LoopData) ->
   {ok, DB} = dets:open_file(LoopData#loopData.dbname, [{type, set}, {file, LoopData#loopData.dbname}]),
-  {ok, DBContentRef} = dets:open_file(LoopData#loopData.dbContentName, [{type, bag}, {file, LoopData#loopData.dbContentName}]),
+  {ok, DBContentRef} = dets:open_file(LoopData#loopData.dbContentName, [{type, set}, {file, LoopData#loopData.dbContentName}]),
+  lists:foreach(fun(N) -> net_adm:ping(N) end, LoopData#loopData.msProdNodes),
   NewLoopData = LoopData#loopData{dbref = DB, dbContentRef = DBContentRef},
+  pg2:create(?MODULE),
+  pg2:join(?MODULE, self()),
   {ok, NewLoopData}.
 
 stop() ->
@@ -39,9 +43,11 @@ handle_cast({inv_update, {ProductId, CountryId, Quantity}}, LoopData) ->
   {noreply, LoopData};
 
 handle_cast({price_update, {ProductId, CountryId, Price}}, LoopData) ->
+  price_update({ProductId, CountryId, Price}, LoopData),
   {noreply, LoopData};
 
-handle_cast({content_update, {ProductId, CountryId, LanguageId, Content}}, LoopData) ->
+handle_cast({content_update, {ProductId, LanguageId, Content}}, LoopData) ->
+  content_update({ProductId, LanguageId, Content}, LoopData),
   {noreply, LoopData};
 
 handle_cast(stop, LoopData) ->
@@ -55,10 +61,23 @@ inv_update({ProductId, CountryId, Quantity}, LoopData) ->
   case Result of
     {error, not_found} -> {error, not_found};
     {ok, {Key, {inv, _ }, Price}} ->
-      ok = dets:insert(db_ref(LoopData), {Key, {inv, Quantity}, Price})
-  end,
+      ok = dets:insert(LoopData#loopData.dbref, {Key, {inv, Quantity}, Price})
+  end.
 
-  {noreply, LoopData}.
+
+price_update({ProductId, CountryId, Price}, LoopData) ->
+  io:format("### price update ~n"),
+  Result = get_product(ProductId, CountryId, LoopData),
+
+  case Result of
+    {error, not_found} -> {error, not_found};
+    {ok, {Key, {inv, Inventory}, {price, _}}} ->
+      ok = dets:insert(LoopData#loopData.dbref, {Key, {inv, Inventory}, {price, Price}})
+  end.
+
+content_update({ProductId, LanguageId, Content}, LoopData) ->
+  io:format("### content update ~n"),
+  dets:insert(LoopData#loopData.dbContentRef, {{ProductId, LanguageId}, Content}).
 
 
 get_product(ProductId, CountryId, LanguageId, LoopData) ->
@@ -72,7 +91,7 @@ get_product(ProductId, CountryId, LanguageId, LoopData) ->
   end.
 
 get_product(ProductId, CountryId, LoopData) ->
-  Result = dets:lookup(db_ref(LoopData), {ProductId, CountryId}),
+  Result = dets:lookup(LoopData#loopData.dbref, {ProductId, CountryId}),
 
   case Result of
     [] -> {error, not_found};
@@ -83,7 +102,6 @@ get_content(ProductId, LanguageId, LoopData) ->
   dets:lookup(LoopData#loopData.dbContentRef, {ProductId, LanguageId}).
 
 
-db_ref(LoopData) -> LoopData#loopData.dbref.
 
 
 

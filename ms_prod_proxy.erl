277c0c56ba947c284ce2c175994d6fc0aa5676bd
marcincembrzynski <marcin@inandi.com>
2018-10-24 09:49:30 +0100

    fix

--- ms_prod_proxy.erl
+++ ms_prod_proxy.erl
@@ -12,17 +12,19 @@ start_link() ->
 init(LoopData) ->
 
   process_flag(trap_exit, true),
-  FunTime = fun(Node, Acc) -> Acc ++ [{Node, timer:tc(net_adm, ping, [Node])}] end,
+  FunTime = fun(Node, Acc) ->
+      case timer:tc(net_adm, ping, [Node]) of
+        {Time, pong} -> Acc ++ [{Node, Time}];
+        {_Time, pang} -> Acc
+      end
+  end,
 
   TimesList = lists:foldl(FunTime, [], LoopData#loopData.msProdNodes),
-
-  SortedTimeList = lists:sort(fun({_, {T1,_}}, {_, {T2,_}}) -> T1 =< T2 end, TimesList),
+  SortedTimeList = lists:sort(fun({_, T1}, {_, T2}) -> T1 =< T2 end, TimesList),
   io:format("TimesList ~p~n", [SortedTimeList]),
   {ClosestNode, _} = lists:nth(1, SortedTimeList),
   io:format("closes node: ~p~n", [ClosestNode]),
   NewLoopData = LoopData#loopData{closestNode = ClosestNode},
-
-  %%lists:foreach(PingNode, LoopData#loopData.msProdNodes),
   pg2:create(ms_prod),
   {ok, NewLoopData}.
 
@@ -35,19 +37,27 @@ get(ProductId, CountryId, LanguageId) ->
   call({get, {ProductId, CountryId, LanguageId}}).
 
 handle_call({get, {ProductId, CountryId, LanguageId}}, _From, LoopData) ->
-  Node = LoopData#loopData.closestNode,
-  {reply, get_product(Node, ProductId, CountryId, LanguageId), LoopData}.
+  {reply, get_product(ProductId, CountryId, LanguageId, LoopData), LoopData}.
 
 handle_cast(stop, LoopData) ->
   {stop, normal, LoopData}.
 
-get_product(Node, ProductId, CountryId, LanguageId) ->
-
-  try ms_prod:get(Node, ProductId, CountryId, LanguageId) of
+get_product(ProductId, CountryId, LanguageId, LoopData) ->
+  ActiveNode = get_active_node(LoopData),
+  try ms_prod:get(ActiveNode, ProductId, CountryId, LanguageId) of
     Response -> Response
   catch
     _:_ ->
-      [Pid|_] = pg2:get_members(ms_prod),
-      NewNode = node(Pid),
-      get_product(NewNode, ProductId, CountryId, LanguageId)
+      get_product(ProductId, CountryId, LanguageId, LoopData)
+  end.
+
+get_active_node(LoopData) ->
+  ActiveNodes = lists:map(fun(P) -> node(P) end, pg2:get_members(ms_prod)),
+  ClosestNode = LoopData#loopData.closestNode,
+  case lists:member(ClosestNode, ActiveNodes) of
+    true ->
+      ClosestNode;
+    false ->
+      [Node|_] = ActiveNodes,
+      Node
   end.

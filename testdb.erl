0bc9922aaf6a5bb115708335a76b2f2db12afae1
marcincembrzynski <marcin@inandi.com>
2018-10-27 10:29:23 +0100

    testdb price and content

--- testdb.erl
+++ testdb.erl
@@ -1,5 +1,5 @@
 -module(testdb).
--export([create/0]).
+-export([create/0,createPrice/1,createContentDB/1]).
 
 % 100000 * 4 = 400000
 create() ->
@@ -13,6 +13,41 @@ create() ->
 	List = [{P,W} || P <- lists:seq(1,100000), W <- [pl,uk,de,fr]],
 	lists:foreach(Populate, List),
 	io:format("info: ~p~n", [dets:info(DB)]).
+
+
+createPrice(PriceDB) ->
+
+	{ok, DB} = dets:open_file(PriceDB, [{type, set}, {file, PriceDB}]),
+	Populate = fun({P,C}) ->
+		dets:insert(DB, {{P,C}, {P, 20}, 1})
+	end,
+
+	List = [{P,C} || P <- lists:seq(1,100000), C <- [pl,uk,de,fr]],
+	lists:foreach(Populate, List),
+	io:format("info: ~p~n", [dets:info(DB)]).
+
+
+createContentDB(ContentDBName) ->
+	{ok, DB} = dets:open_file(ContentDBName, [{type, set}, {file, ContentDBName}]),
+
+	Languages = [pl,en,de,fr],
+	ProductLanguage = [{P,L} || P <- lists:seq(1,100000), L <- Languages],
+
+	PopulateContent = fun({ProductId, LanguageId}) ->
+		Title = string:concat(string:concat("Test Title Product ", integer_to_list(ProductId)), atom_to_list(LanguageId)),
+		Description = string:concat(string:concat("Test Description Product ", integer_to_list(ProductId)), atom_to_list(LanguageId)),
+		Key = {ProductId, LanguageId},
+		Value = [{title, Title},{description, Description}],
+		Version = 1,
+		dets:insert(DB, {Key, Value, Version})
+	end,
+
+	lists:foreach(PopulateContent, ProductLanguage),
+
+	io:format("info: ~p~n", [dets:info(DB)]),
+	dets:close(DB).
+
+
 
 
 
